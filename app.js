587a6f63a14066bc60bff1bfbd3f5d7fb65663bd
Louis Maiorino <lmlmlou@gmail.com>
2024-01-25 22:11:17 -0500

    1-25 added mongoose db

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const mongoose = require("mongoose");
 
 const { PORT = 3001 } = process.env;
 const app = express();
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/wtwr_db",
+  (r) => {
+    console.log("connected to DB");
+  },
+  (e) => console.log("DB error", e),
+);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
