587a50de92f8bfae0cb76a4a7ba68c5946c85300
Louis Maiorino <lmlmlou@gmail.com>
2024-01-29 21:24:33 -0500

    1-29

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,23 +6,6 @@ module.exports = {
   },
   // update the extensions
   extends: ["eslint:recommended", "airbnb-base", "prettier"],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  extends: "airbnb-base",
   rules: {
     "no-underscore-dangle": ["error", { allow: ["foo_", "_bar"] }],
   },
