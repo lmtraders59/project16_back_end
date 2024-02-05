0c6208edb24f82a73795f42d886371a39d565e19
Louis Maiorino <lmlmlou@gmail.com>
2024-02-04 21:26:49 -0500

    postman error 2-4

--- controllers/users.js
+++ controllers/users.js
@@ -1,57 +1,74 @@
 const User = require("../models/user");
 
-const BadRequestError = require("../utils/errors/badRequestError");
-const NotFoundError = require("../utils/errors/notFoundError");
+// const BadRequestError = require("../utils/errors/badRequestError");
+// const NotFoundError = require("../utils/errors/notFoundError");
 
-// returns all users
-const getUsers = (req, res, next) => {
-  const { name, avatar } = req.body;
-  const { _id } = req.user;
-};
-
-// returns a user by _id
-const getUser = (req, res, next) => {
-  const { _id } = req.user;
-
-  User.findById({ _id })
-    .then((user) => {
-      if (!user) {
-        next(new NotFoundError("User not found"));
-      } else {
-        res.send(user);
-      }
-    })
+// GET users returns all users
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
     .catch((err) => {
-      if (err.name === "CastError") {
-        next(new BadRequestError("Bad request, invalid ID"));
-      } else {
-        next(err);
-      }
+      console.log(err);
+      return res.status(500).send({ message: err.message });
     });
 };
+
+const createUser = (req, res) => {
+  const { name, avatar } = req.body;
+
+  User.create({ name, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      console.log(err);
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+// returns a user by _id
+// const getUser = (req, res, next) => {
+//   const { _id } = req.user;
+
+//   User.findById({ _id })
+//     .then((user) => {
+//       if (!user) {
+//         next(new NotFoundError("User not found"));
+//       } else {
+//         res.send(user);
+//       }
+//     })
+//     .catch((err) => {
+//       if (err.name === "CastError") {
+//         next(new BadRequestError("Bad request, invalid ID"));
+//       } else {
+//         next(err);
+//       }
+//     });
+// };
 
 // creates a new user
-const newUser = (req, res, next) => {
-  const { name, avatar } = req.body;
-  const { _id } = req.user;
-  User.findByIdAndUpdate(
-    { _id },
-    { name, avatar },
-    { new: true, runValidators: true },
-  )
-    .then((user) => {
-      res.send(user);
-    })
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        next(new BadRequestError("Bad request, invalid data"));
-      } else {
-        next(err);
-      }
-    });
-};
+// const newUser = (req, res, next) => {
+//   const { name, avatar } = req.body;
+//   const { _id } = req.user;
+//   User.findByIdAndUpdate(
+//     { _id },
+//     { name, avatar },
+//     { new: true, runValidators: true },
+//   )
+//     .then((user) => {
+//       res.send(user);
+//     })
+//     .catch((err) => {
+//       if (err.name === "ValidationError") {
+//         next(new BadRequestError("Bad request, invalid data"));
+//       } else {
+//         next(err);
+//       }
+//     });
+// };
 
 module.exports = {
-  getUser,
-  newUser,
+  getUsers,
+  createUser,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 
+const { getUsers, createUser } = require("../controllers/users");
+
 // GET /users — returns all users
-router.get("/", () => console.log("GET users"));
+router.get("/", getUsers);
 
 // GET /users/:userId - returns a user by _id
 router.get("/:userId", () => console.log("GET users by ID"));
 
 // POST /users — creates a new user
-router.post("/", () => console.log("POST users"));
+router.post("/", createUser);
 
 module.exports = router;
